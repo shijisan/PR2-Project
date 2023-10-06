6a59f094f7c45e93d9193481f0c7ca628f491f5e
shijisan <115002067+shijisan@users.noreply.github.com>
2023-10-06 10:51:19 +0800

    Delete js directory

--- js/hero.js
+++
@@ -1,4 +0,0 @@
-function redirectToQuiz() {
-    // Redirect to the quiz page
-    window.location.href = "html/game.html"; // Replace "quiz.html" with the URL of the quiz page you want to navigate to
-}
